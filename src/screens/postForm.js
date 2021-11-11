6efbf1edcf982445619880d90aa2c56ae5f50bb6
Mora Baylac <morabaylac@MacBook-Pro-de-Mora.local>
2021-11-11 07:39:54 -0500

    organizando archivos

--- src/screens/postForm.js
+++ src/screens/postForm.js
@@ -1,70 +1,85 @@
 import React, {Component} from 'react'
 import {Text, TouchableOpacity, View, StyleSheet, Image, ActivityIndicator, FlatList, TextInput} from 'react-native'
 import {auth, db} from '../firebase/config'
+import MyCamera from '../components/MyCamera';
+
+
 
 class PostForm extends Component{
     constructor(props){
         super(props);
-        this.state = {
-            textoPost:'',
+        this.state={
+            textoPost: '',
+            showCamera: true,
         }
     }
-
     submitPost(){
+        console.log('posteando..');
         db.collection('posts').add({
             owner: auth.currentUser.email,
             texto: this.state.textoPost,
             createdAt: Date.now(),
+            photo: this.state.url,
         })
-        .then( () => {
+        .then(() =>{ 
             this.setState({
                 textoPost: ''
             })
-            //Redireccion
+            // Redirección
             this.props.drawerProps.navigation.navigate('Home')
         })
-        .catch()
+        .catch( e => console.log(e))
     }
-
+    onImageUpload(url){
+        this.setState({
+            showCamera: false,
+            url: url,
+        })
+    }
+    
     render(){
         return(
-            <View>
-                <Text>Nuevo Post</Text>
+            <View style={styles.container}>
+                {
+                this.state.showCamera ?
+                <MyCamera onImageUpload={(url)=> {this.onImageUpload(url)}}/>:
                 <View style={styles.formContainer}>
-                    <TextInput
-                        style={styles.field}
-                        keyboardType='default'
-                        placeholder='Escribe Aqui'
-                        onChangeText={text => this.setState({textoPost: text})}
-                        multiline
-                        value={this.state.textoPost}
-                    />
-                    <Text>{this.props.errorMessage}</Text>
-                    <TouchableOpacity style={styles.touchable} onPress={() => this.submitPost() } > 
-                        <Text style={styles.text}>Guardar</Text>
-                    </TouchableOpacity>
-                </View>
+                <TextInput style={styles.field} 
+                keyboardType='default'
+                multiline
+                placeholder='Escribí aquí'
+                onChangeText={ text => this.setState({textoPost:text}) }/>
+            
+                <TouchableOpacity style={styles.boton} onPress={() => this.submitPost()}>
+                <Text style={styles.textboton}> Guardar </Text> 
+                </TouchableOpacity> 
             </View>
+            }
+        </View>
         )
     }
-}
-
-const styles = StyleSheet.create({
-    formContainer: {
-        padding: 10,
-        marginTop: 10,
-    },
-    field: {
-        height: 100,
-        paddingVertical: 15,
-        paddingHorizontal: 10,
-        borderWidth: 1,
-        borderStyle: 'solid',
-        backgroundColor: '#ccc',
-        borderRadius: 6,
-        marginVertical: 10,
-    },
-    touchable: {
+    }
+         
+    const styles = StyleSheet.create({
+        container:{
+            flex: 1,
+        },
+        formContainer: {
+            paddingHorizontal: 10,
+            marginBottom: 20,
+            
+        },
+           field: {
+            height: 20,
+            paddingVertical: 15,
+            paddingHorizontal: 10,
+            borderWidth: 1,
+            borderColor:'#ccc',
+            borderStyle: 'solid',
+            borderRadius: 6,
+            marginVertical: 10,
+           },
+       boton:{
         backgroundColor: '#28a745',
         paddingHorizontal: 10,
         paddingVertical: 6,
@@ -73,10 +88,10 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderStyle: 'solid',
         borderColor: '#28a745',
-    },
-    text: {
-        color: '#fff'
-    }
-})
+       },
+       textboton:{
+           color: '#fff',
+       },
+     }) 
 
-export default PostForm
+     export default PostForm
